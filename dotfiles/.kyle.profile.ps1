e21e79bb90ed4c6360ff6dbaecb1d3168de22cdd
KyleMit <kmitofsky@stackoverflow.com>
2024-02-05 14:49:08 -0500

    2/5

--- dotfiles/.kyle.profile.ps1
+++ dotfiles/.kyle.profile.ps1
@@ -36,3 +36,15 @@ function Start-IISSites {
 
     Get-IISSite | Where-Object { $_.State -eq 'Stopped' } | ForEach-Object { $_.Start() }
 }
+
+function git-revert-eol-only {
+    $root = git rev-parse --show-toplevel
+
+    $allChanges = git diff --name-status 2>&1
+
+    $blanks = $allChanges | Select-String "warning: in the working copy of '(.*)', LF" | ForEach-Object { $_.Matches.Groups[1].value }
+    $changed = $allChanges | Select-String "M\s+(.*)" | ForEach-Object { $_.Matches.Groups[1].value }
+    $blankOnly = $blanks | Where-Object { $changed -notcontains $_ }
+
+    $blankOnly | ForEach-Object { git restore "$root/$_" }
+}
